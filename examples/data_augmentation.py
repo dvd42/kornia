34d799d2091e8e88467bb3c31958a51bd36e6306
dvd42 <diegovd0296@gmail.com>
2019-11-12 19:59:49 +0100

    example data augmentation

--- examples/data_augmentation.py
+++ examples/data_augmentation.py
@@ -2,7 +2,7 @@
 Data augmentation on the GPU
 ============================
 
-In this data you learn how to use `kornia` modules in order to perform the data augmentatio on the GPU in batch mode.
+In this data you learn how to use `kornia` modules in order to perform the data augmentation on the GPU in batch mode.
 """
 
 ################################
@@ -11,30 +11,9 @@ In this data you learn how to use `kornia` modules in order to perform the data 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-
-# from: https://gist.github.com/edgarriba/a781de516c508826f79568d08598efdb
-
-
-class DummyDataset(Dataset):
-    def __init__(self, data_root=None):
-        self.data_root = data_root
-        self.data_index = self.build_index(self.data_root)
-
-    def build_index(self, data_root):
-        return range(10)
-
-    def __len__(self):
-        return len(self.data_index)
-
-    def __getitem__(self, idx):
-        # get data sample
-        sample = self.data_index[idx]
-
-        # load data, NOTE: modify by cv2.imread(...)
-        image = torch.rand(3, 240, 320)
-        label = torch.rand(1, 240, 320)
-        return dict(images=image, labels=label)
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
 
 ################################
 # 2. Define the data augmentation operations
@@ -43,9 +22,9 @@ class DummyDataset(Dataset):
 import kornia
 
 transform = nn.Sequential(
-    kornia.color.AdjustBrightness(0.5),
-    kornia.color.AdjustGamma(gamma=2.),
-    kornia.color.AdjustContrast(0.7),
+    kornia.color.Normalize(torch.tensor([0.485, 0.456, 0.406]), torch.tensor([0.229, 0.224, 0.225])),
+    kornia.augmentation.ColorJitter(brightness=0.5, contrast=1.2, hue=0.5, saturation=0.3),
+    kornia.augmentation.RandomHorizontalFlip(0.5),
 )
 
 ################################
@@ -56,16 +35,18 @@ device = torch.device('cpu')
 print(f"Running with device: {device}")
 
 # create the dataloader
-dataset = DummyDataset()
+# dataset = CIFAR10(root="data", download=True, transform=ToTensor())
+dataset = CIFAR10(root="data", download=True, transform=kornia.image_to_tensor)
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
 # get samples and perform the data augmentation
-for i_batch, sample_batched in enumerate(dataloader):
-    images = sample_batched['images'].to(device)
-    labels = sample_batched['labels'].to(device)
+# import ipdb; ipdb.set_trace()  # BREAKPOINT
+for img, target in dataloader:
+
+    images = img.to(device)
+    labels = target.to(device)
 
     # perform the transforms
     images = transform(images)
-    labels = transform(labels)
 
     print(f"Iteration: {i_batch} Image shape: {images.shape}")
